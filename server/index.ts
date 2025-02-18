e88ed3c267c3cd3daec80394e1f38e043c0f85bf
Prince <connect@candidate.live>
2025-02-19 00:19:40 +0530

    Done

--- server/index.ts
+++ server/index.ts
@@ -19,6 +19,7 @@ app.use((req, res, next) => {
 
   res.on("finish", () => {
     const duration = Date.now() - start;
+    console.log("duration", duration);
     if (path.startsWith("/api")) {
       let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
       if (capturedJsonResponse) {
